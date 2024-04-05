a010859c6d5fe29302e6b58cb5a1ae96e62740b6
Ritesh sirohi <Sirohiritesh09@gmail.com>
2024-04-05 11:09:30 +0530

    Completed userController

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -41,7 +41,62 @@ exports.registerController = async (req, res) => {
   
 
 
-exports.getAllUsers = () => {};
+  exports.getAllUsers = async (req, res) => {
+    try {
+      const users = await userModel.find({});
+      return res.status(200).send({
+        userCount: users.length,
+        success: true,
+        message: "all users data",
+        users,
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send({
+        success: false,
+        message: "Error In Get ALl Users",
+        error,
+      });
+    }
+  };
 
 
-exports.loginController = () => {};
+  exports.loginController = async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      //validation
+      if (!email || !password) {
+        return res.status(401).send({
+          success: false,
+          message: "Please provide email or password",
+        });
+      }
+      const user = await userModel.findOne({ email });
+      if (!user) {
+        return res.status(200).send({
+          success: false,
+          message: "email is not registerd",
+        });
+      }
+      //password
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(401).send({
+          success: false,
+          message: "Invlid username or password",
+        });
+      }
+      return res.status(200).send({
+        success: true,
+        messgae: "login successfully",
+        user,
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send({
+        success: false,
+        message: "Error In Login Callcback",
+        error,
+      });
+    }
+  };
