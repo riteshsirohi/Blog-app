b5b98f8d84cf9b044e9b06291ce69a2b199320b2
Ritesh sirohi <Sirohiritesh09@gmail.com>
2024-04-06 18:42:11 +0530

    Add blog routes and controller

--- server.js
+++ server.js
@@ -1,28 +1,38 @@
-const express = require('express')
-const cors = require('cors')
-const morgan = require('morgan')
-const colors = require('colors')
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const colors = require("colors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
-
+//env config
 dotenv.config();
 
-const userRoutes = require('./routes/user.routes.js')
+//router import
+const userRoutes = require("./routes/user.routes.js");
+const blogRoutes = require("./routes/blog.routes.js");
 
+//mongodb connection
 connectDB();
 
+//rest object
 const app = express();
 
+//middelwares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
 
-app.use(cors())
-app.use(express.json())
-app.use(morgan('dev'))
+//routes
+app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/blog", blogRoutes);
 
-app.use('/api/v1/users',userRoutes);
-
-const PORT = process.env.PORT || 8080
-
-app.listen(PORT,()=>{
-    console.log(`server is running ${process.env.DEV_MODE} on ${PORT}`.bgCyan.white);
-})
+// Port
+const PORT = process.env.PORT || 8080;
+//listen
+app.listen(PORT, () => {
+  console.log(
+    `Server Running on ${process.env.DEV_MODE} mode port no ${PORT}`.bgCyan
+      .white
+  );
+});
